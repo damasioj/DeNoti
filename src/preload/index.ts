ac1b9286ada7a7490cd3b821bf76540926a40818
damasioj <joshua.damasio@gmail.com>
2026-06-03 16:51:28 +0900

    make welcome.md default file

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,9 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('api', {
+  getWelcomePath: (): Promise<string> =>
+    ipcRenderer.invoke('get-welcome-path'),
+
   getTabs: (): Promise<unknown> =>
     ipcRenderer.invoke('get-tabs'),
 
